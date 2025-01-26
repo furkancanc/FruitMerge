318c9e717301c9246f6bc472a749145d7a7302a8
furkancanc <fcc.ceylan@gmail.com>
2025-01-26 14:54:16 +0300

    GameOver Timer

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -6,6 +6,12 @@ public class GameOverManager : MonoBehaviour
     [SerializeField] private GameObject deadLine;
     [SerializeField] private Transform fruitsParent;
 
+    [Header(" Timer ")]
+    [SerializeField] private float durationThreshold;
+    private float timer;
+    private bool timerOn;
+    private bool isGameOver;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,7 +21,63 @@ public class GameOverManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckForGameOver();
+        if (!isGameOver)
+        {
+            ManageGameOver();
+        }
+    }
+
+    private void ManageGameOver()
+    {
+        if (timerOn)
+        {
+            ManageTimerOn();
+            
+        }
+        else
+        {
+            if (IsFruitAboveLine())
+            {
+                StartTimer();
+            }
+        }
+    }
+
+    private void ManageTimerOn()
+    {
+        timer += Time.deltaTime;
+
+        Debug.Log("Timer: " + timer);
+
+        if (!IsFruitAboveLine())
+        {
+            StopTimer();
+        }
+
+        if (timer >= durationThreshold)
+        {
+            GameOver();
+        }
+    }
+
+    private bool IsFruitAboveLine()
+    {
+        for (int i = 0; i < fruitsParent.childCount; ++i)
+        {
+            Fruit fruit = fruitsParent.GetChild(i).GetComponent<Fruit>();
+
+            if (!fruit.HasCollided())
+            {
+                continue;
+            }
+
+            if(IsFruitAboveLine(fruitsParent.GetChild(i)))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private void CheckForGameOver()
@@ -29,15 +91,34 @@ public class GameOverManager : MonoBehaviour
                 continue;
             }
 
-            CheckIfFruitAboveLine(fruitsParent.GetChild(i));
+            IsFruitAboveLine(fruitsParent.GetChild(i));
         }
     }
 
-    private void CheckIfFruitAboveLine(Transform fruit)
+    private bool IsFruitAboveLine(Transform fruit)
     {
         if (fruit.position.y > deadLine.transform.position.y)
         {
-            Debug.Log("GameOver");
+            return true;
         }
+
+        return false;
+    }
+
+    private void StartTimer()
+    {
+        timer = 0;
+        timerOn = true;
+    }
+
+    private void StopTimer()
+    {
+        timerOn = false;
+    }
+
+    private void GameOver()
+    {
+        Debug.Log("Game Over!");
+        isGameOver = true;
     }
 }
