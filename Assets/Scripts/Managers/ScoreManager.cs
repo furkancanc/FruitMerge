591aaa933b7e0cd22d4b44332f6de6df91396a4e
furkancanc <fcc.ceylan@gmail.com>
2025-02-18 22:39:15 +0300

    Adding A Best Score

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -5,24 +5,34 @@ public class ScoreManager : MonoBehaviour
 {
     [Header("Elements")]
     [SerializeField] private TextMeshProUGUI gameScoreText;
+    [SerializeField] private TextMeshProUGUI menuBestScoreText;
 
     [Header("Settings")]
     [SerializeField] private float scoreMultiplier;
     private int score;
+    private int bestScore;
+
+    [Header("Data")]
+    private const string bestScoreKey = "bestScoreKey";
 
     private void Awake()
     {
+        LoadData();
+
         MergeManager.onMergeProcessed += MergeProcessedCallback;
+        GameManager.onGameStateChanged += GameStateChangedCallback;
     }
 
     private void OnDestroy()
     {
         MergeManager.onMergeProcessed -= MergeProcessedCallback;
+        GameManager.onGameStateChanged -= GameStateChangedCallback;
     }
 
     private void Start()
     {
-        UpdateScore();
+        UpdateScoreText();
+        UpdateBestScoreText();
     }
 
     private void MergeProcessedCallback(FruitType fruitType, Vector2 unused)
@@ -30,11 +40,45 @@ public class ScoreManager : MonoBehaviour
         int scoreToAdd = (int)fruitType;
         score += (int)(scoreToAdd * scoreMultiplier);
 
-        UpdateScore();
+        UpdateScoreText();
     }
 
-    private void UpdateScore()
+    private void UpdateScoreText()
     {
         gameScoreText.text = score.ToString();
+    }
+
+    private void UpdateBestScoreText()
+    {
+        menuBestScoreText.text = bestScore.ToString();
+    }
+
+    private void GameStateChangedCallback(GameState gameState)
+    {
+        switch (gameState)
+        {
+            case GameState.GameOver:
+                CalculateBestScore();
+                break;
+        }
+    }
+
+    private void CalculateBestScore()
+    {
+        if (score >= bestScore)
+        {
+            bestScore = score;
+            SaveData();
+        }
+    }
+
+    private void LoadData()
+    {
+        bestScore = PlayerPrefs.GetInt(bestScoreKey);
+    }
+
+    private void SaveData()
+    {
+        PlayerPrefs.SetInt(bestScoreKey, bestScore);
     }
 }
